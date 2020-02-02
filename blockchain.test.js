9acd309a7c9da108ebdd40927e5e3d33db64c754
Ian Seng <ianseng@outlook.com>
2020-02-02 15:23:08 -0500

    implement unit test for chain replcement function

--- blockchain.test.js
+++ blockchain.test.js
@@ -20,20 +20,36 @@ describe("Blockchain", () => {
     expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
   });
 
-  it('validates a valid chain', ()=>{
-      bc2.addBlock('foo')
-      expect(bc.isValidChain(bc2.chain)).toBe(true)
+  it("validates a valid chain", () => {
+    bc2.addBlock("foo");
+    expect(bc.isValidChain(bc2.chain)).toBe(true);
+  });
+  it("invalidate a chain with a corrupt genesis block", () => {
+    bc2.chain[0].data = "bad data";
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
 
+  it("invalidate a corrupt chain", () => {
+    bc2.addBlock("foo");
+    bc2.chain[1].data = "Not foo";
+
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+
+
+  it('replaces the chain with a valid chain', () => {
+      bc2.addBlock('goo');
+      bc.replaceChain(bc2.chain);
+
+      expect(bc.chain).toEqual(bc2.chain);
+  }) 
+
+  it('does not replace the chain with one or less than or equal to length', () => {
+    bc.addBlock('foo');
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).not.toEqual(bc2.chain);
   })
-  it ('invalidate a chain with a corrupt genesis block', () => {
-      bc2.chain[0].data = 'bad data';
-      expect(bc.isValidChain(bc2.chain)).toBe(false)
-  })
 
-  it ('invalidate a corrupt chain', () => {
-    bc2.addBlock('foo');
-    bc2.chain[1].data = 'Not foo'
 
-    expect(bc.isValidChain(bc2.chain)).toBe(false)
-})
 });

dbb80aa41351f854d600ade6a871fc3724bbc248
Ian Seng <ianseng@outlook.com>
2020-02-09 22:47:16 -0500

    user is able to send message

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const WebSocket = require('ws')
-server = new WebSocket.Server({port: 3001})
+const WebSocket = require("ws"),
+  server = new WebSocket.Server({ port: 5001, });
 
-function broadcast(data){
-    server.clients.forEach(ws => {
-        ws.send(data)
-    })
+function broadcast(data) {
+  server.clients.forEach(ws => {
+    ws.send(data);
+  });
 }
 
-server.on('connection', ws => {
-    ws.on('message', data => {
-        broadcast(data);
-    })
-})
+server.on("connection", ws => {
+  ws.on("message", data => {
+    broadcast(data);
+  });
+});
